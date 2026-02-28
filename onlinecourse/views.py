3b9e73f8f73723a73964a8bbcd4c40765e26336a
prvmeenu <prvmeenu@gmail.com>
2026-02-27 23:49:04 -0500

    changes made to show the  result

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,9 +149,12 @@ def show_exam_result(request, course_id, submission_id):
     questions = course.question_set.all()
 
     for question in questions:
-        question.choice_set.filter(is_correct=True) # Get all correct choices for the question selected_choices = choices.filter(question=question) # Get the user's selected choices for the question
+        correct_choices = question.choice_set.filter(is_correct=True) # Get all correct choices for the question 
+        selected_choices = choices.filter(question=question) # Get the user's selected choices for the question
 
-    # Check if the selected choices are the same as the correct choices if set(correct_choices) == set(selected_choices): total_score += question.grade # Add the question's grade only if all correct answers are selected
+    # Check if the selected choices are the same as the correct choices
+    if set(correct_choices) == set(selected_choices): 
+        total_score += question.grade # Add the question's grade only if all correct answers are selected
 
     context['course'] = course 
     context['grade'] = total_score 
